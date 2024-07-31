fcbfe52ee1014a3137fee89cbc3a69fc77d23fa3
UttU28 <utsavmaan28@gmail.com>
2024-07-31 14:21:20 -0500

    JSK

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
-# provider "azurerm" {
-#   features {}
-# }
+provider "azurerm" {
+  features {}
+}
 
-# data "azurerm_resource_group" "example" {
-#   name = "thisresourcegroup"
-#   #   location = "East US"
-# }
+data "azurerm_resource_group" "example" {
+  name = "thisresourcegroup"
+  #   location = "East US"
+}
 
-# resource "azurerm_service_plan" "example" {
-#   name                = "thisismyserviceplan"
-#   resource_group_name = data.azurerm_resource_group.example.name
-#   location            = "East US"
-#   os_type             = "Linux"
-#   sku_name            = "B1"
-# }
+resource "azurerm_service_plan" "example" {
+  name                = "thisismyserviceplan"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = "East US"
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
 
-# resource "azurerm_linux_web_app" "example" {
-#   name                = "thisismywebapp123321"
-#   resource_group_name = data.azurerm_resource_group.example.name
-#   location            = azurerm_service_plan.example.location
-#   service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_linux_web_app" "example" {
+  name                = "dicesaralapply"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
 
-#   site_config {
-#     always_on = true
-#     application_stack {
-#       docker_image_name        = "thisacr.azurecr.io/imagename:latest"
-#       docker_registry_username = "thisacr"
-#       docker_registry_password = "U9+ivfherZPq3+UWDnj1fxftpOqWUgXqspIc90YYFI+ACRBkerUy"
-#     }
-#   }
+  site_config {
+    always_on = true
+    application_stack {
+      docker_image_name        = "thisacr.azurecr.io/imagename:latest"
+      docker_registry_username = "thisacr"
+      docker_registry_password = "U9+ivfherZPq3+UWDnj1fxftpOqWUgXqspIc90YYFI+ACRBkerUy"
+    }
+  }
 
-# }
+}
 
 # resource "null_resource" "restart_web_app" {
 #   depends_on = [azurerm_linux_web_app.example]
