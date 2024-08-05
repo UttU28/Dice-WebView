e6ae0efd08785eb95c14b73c74bc2cd2c3182dd2
UttU28 <utsavmaan28@gmail.com>
2024-08-05 09:42:15 -0500

    JSK

--- basic.sql
+++ basic.sql
@@ -1,2 +1,9 @@
--- SELECT * from allData where [description] LIKE '%python%';
+-- -- SELECT * from allData where [description] LIKE '%python%';
 select COUNT(*) from myQueue;
+select COUNT(*) from applyQueue;
+
+-- DELETE FROM myQueue
+-- WHERE timeOfArrival < DATEADD(DAY, -1, GETDATE());
+
+
+-- select COUNT(*) from myQueue;
